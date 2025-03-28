0c2eccb33db0c63b8c64442171d2efaa35c51efb
Jamie Forth <j.forth@gold.ac.uk>
2025-03-28 00:10:13 +0000

    fix typo in mne data channel types and add non-data channel types

--- src/pdxdf/constants.py
+++ src/pdxdf/constants.py
@@ -1,6 +1,6 @@
 """General definitions."""
 
-# https://mne.tools/stable/glossary.html#term-data-channels
+# https://mne.tools/stable/documentation/glossary.html#term-data-channels
 data_channel_types = [
     "mag",
     "grad",
@@ -8,12 +8,36 @@ data_channel_types = [
     "csd",
     "seeg",
     "ecog",
-    "dbshbo",
+    "dbs",
+    "hbo",
     "hbr",
     "fnirs_cw_amplitude",
     "fnirs_fd_ac_amplitude",
     "fnirs_fd_phase",
     "fnirs_od",
-] + ["data"]
+]
+
+# https://mne.tools/stable/documentation/glossary.html#term-non-data-channels
+non_data_channel_types = [
+    "eog",
+    "ecg",
+    "resp",
+    "emg",
+    "ref_meg",
+    "misc",
+    "stim",
+    "chpi",
+    "exci",
+    "ias",
+    "syst",
+    "bio",
+    "temperature",
+    "gsr",
+    "gof",
+    "dipole",
+    "eyegaze",
+    "pupil",
+    "whitened",
+]
 
 microvolts = ("microvolt", "microvolts", "uV", "µV", "μV")
