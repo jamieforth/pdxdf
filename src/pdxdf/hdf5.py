b29f94dc3844d2d051aa5f6d78a2bdced23a590b
Jamie Forth <j.forth@gold.ac.uk>
2025-07-28 06:51:58 +0100

    hdf5: add key parameter

--- src/pdxdf/hdf5.py
+++ src/pdxdf/hdf5.py
@@ -1,25 +1,25 @@
 import pandas as pd
 
-def save(path, df):
+def save(path, df, key="stream"):
     if "stream_info" not in df.attrs:
         raise ValueError("DataFrame missing attribute: stream_info.")
     info = df.attrs["stream_info"]
     if (df.dtypes != info.channel_format).any():
         df = df.astype(info.channel_format)
     with pd.HDFStore(path) as store:
-        store.put("stream", df, format="table")
-        store.get_storer("stream").attrs.stream_info = info
+        store.put(key, df, format="table")
+        store.get_storer(key).attrs.stream_info = info
 
 
-def load(path, start=0, stop=None):
+def load(path, key="stream", start=0, stop=None):
     with pd.HDFStore(path, mode="r") as store:
-        df = pd.read_hdf(store, "stream", start=start, stop=stop)
-        info = store.get_storer("stream").attrs.stream_info
+        df = pd.read_hdf(store, key, start=start, stop=stop)
+        info = store.get_storer(key).attrs.stream_info
         df.attrs["stream_info"] = info
     return df
 
 
-def load_stream_info(path):
+def load_stream_info(path, key="stream"):
     with pd.HDFStore(path, mode="r") as store:
-        info = store.get_storer("stream").attrs.stream_info
+        info = store.get_storer(key).attrs.stream_info
         return info
